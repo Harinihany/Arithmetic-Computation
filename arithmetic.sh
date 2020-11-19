949d130566b934da85a9c4e210b0d4e673b745d1
Harinihany <harinihany03@gmail.com>
2020-11-19 19:11:39 +0530

    added:UC-6

--- arithmetic.sh
+++ arithmetic.sh
@@ -4,32 +4,38 @@ read -p "enter number1: " a
 read -p "enter number2: " b
 read -p "enter number3: " c
 
-result=$(( ($a + $b) * $c ))
+result1=$(( ($a + $b) * $c ))
 
-echo "result is $result"
+echo "result is $result1"
 echo "************"
 
 
 read -p "enter number1: " a
 read -p "enter number2: " b
 read -p "enter number3: " c
-result=$(( ($a * $b) + $c))
-echo "result is $result"
+result2=$(( ($a * $b) + $c))
+echo "result is $result2"
 echo "*************"
 
 read -p "enter number1: " a
 read -p "enter number2: " b
 read -p "enter number3: " c
-result=$(( ($c + $a) / $b))
-echo "result is $result"
+result3=$(( ($c + $a) / $b))
+echo "result is $result3"
 echo "***********"
 
 read -p "enter number1: " a
 read -p "enter number2: " b
 read -p "enter number3: " c
-result=$(( ($a % $b) + $c))
-echo "result is $result"
+result4=$(( ($a % $b) + $c))
+echo "result is $result4"
+echo "*********"
 
+declare -A dict
+dict["num1"]=$result1
+dict["num2"]=$result2
+dict["num3"]=$result3
+dict["num4"]=$result4
 
-
+echo ${dict[@]}
 
